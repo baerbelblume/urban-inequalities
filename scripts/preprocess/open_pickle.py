8ee0737ce75f4e2af64bd43c4dacbbd26ca35f11
Barbara Metzler <phillipachloe@gmail.com>
2019-08-30 08:03:07 +0200

    Update open_pickle.py

--- scripts/preprocess/open_pickle.py
+++ scripts/preprocess/open_pickle.py
@@ -10,45 +10,12 @@ df = pickle.load(pickle_in)
 #['index', 'pcd', 'pcd2', 'pcds', 'dointr', 'doterm', 'oscty', 'oslaua', 'osward', 'usertype', 'oseast1m', 'osnrth1m', 'osgrdind', 'oshlthau', 'hro', 'ctry', 'gor', 'streg', 'pcon', 'eer', 'teclec', 'ttwa', 'pct', 'nuts', 'psed', 'cened', 'edind', 'oshaprev', 'lea', 'oldha', 'wardc91', 'wardo91', 'ward98', 'statsward', 'oa01', 'casward', 'park', 'lsoa01', 'msoa01', 'ur01ind', 'oac01', 'oldpct', 'oa11', 'lsoa11', 'msoa11', 'parish', 'wz11', 'ccg', 'bua11', 'buasd11', 'ru11ind', 'oac11', 'lat', #'long', 'lep1', 'lep2', 'pfa', 'imd', 'gsv_lat', 'gsv_lng', 'gsv_pano_id', 'gsv_metastatus', 'gsv_copyright', 'gsv_date', 'status', 'img_id', 'pano_id', 'Population', 'Household', 'lsoa-age-mean', 'lsoa-age-median', 'perc-lsoa-generalhealth-bad-or-verybad', 'decile-lsoa-generalhealth-bad-or-verybad', 'perc-lsoa-occupancy-rating--1orless', 'decile-lsoa-occupancy-rating--1orless', 'perc-lsoa-hrp-unemployment-percent', 'decile-lsoa-hrp-unemployment', 'perc-lsoa-qual-below-level2', 'decile-lsoa-
 # below-level2', 'dep-income-rank-uk-lsoa', 'dep-employment-rank-uk-lsoa', 'dep-education-rank-uk-lsoa', 'dep-health-rank-uk-lsoa', 'dep-crime-rank-uk-lsoa', 'dep-housing-rank-uk-lsoa', 'dep-liv-env-rank-uk-lsoa', 'dep-income-decile-london-lsoa', 'dep-employment-decile-london-lsoa', 'dep-education-decile-london-lsoa', 'dep-health-decile-london-lsoa', 'dep-crime-decile-london-lsoa', 'dep-housing-decile-london-lsoa', 'dep-liv-env-decile-london-lsoa', 'mean-income-12-13-lsoa', 'mean-income-decile-#london-lsoa', 'median-income-12-13-lsoa', 'median-income-decile-london-lsoa']
 
+df = df.groupby(['img_id']).sum())
 
-object_test = pd.read_csv('../df_test.csv')
-object_test['img_id'] = object_test['name'].str.split('_').str[0]
-
-
-obj_df = pd.crosstab(object_test.img_id, object_test.object)
-obj_df = obj_df.groupby(['img_id']).sum())
-#[78245 rows x 78 columns]
-
-#print (object_test.head(50))
+#print (df.head(50))
 
 outcomes = df[['index', 'lat', 'long', 'dep-income-decile-london-lsoa', 'mean-income-12-13-lsoa',
     'mean-income-decile-london-lsoa', 'median-income-12-13-lsoa', 'median-income-decile-london-lsoa',
     'img_id', 'pano_id', 'Population', 'Household']]
 
-#numerical
-mean_income_log = outcomes[['mean-income-12-13-lsoa']].apply(np.log)
-mean_income = outcomes[['mean-income-12-13-lsoa']]
 
-#categorical: 1-10
-income_dep = outcomes[['dep-income-decile-london-lsoa']]
-
-dataframe = outcomes[['index', 'img_id', 'mean-income-12-13-lsoa', 'dep-income-decile-london-lsoa']]
-
-### one-hot-encoder
-from sklearn.preprocessing import LabelEncoder
-
-le_objects = LabelEncoder()
-
-
-plt.title('log-transformed mean-income-12-13-lsoa')
-sns.distplot(mean_income_log.values, axlabel='mean income')
-plt.show()
-
-plt.title('mean-income-12-13-lsoa')
-sns.distplot(mean_income.values, axlabel='mean income')
-plt.show()
-
-#print (income_dep.describe())
-#sns.distplot(income_dep, kde=False, bins=10, axlabel='object label')
-#plt.title('dep-income-decile-london-lsoa')
-#plt.show()
